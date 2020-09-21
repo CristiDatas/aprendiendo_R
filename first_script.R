65728069f72eac0d19b4d1fc0d4e3d6e04fe6741
CristiDatas <cristidatas@gmail.com>
2020-09-21 17:18:56 +0200

    peobando el primer script de R

---
+++ first_script.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+# para el ejercicio de hoy copia las siguientes líneas en tu script
+
+# cargamos los datos
+data("mtcars")
+head(mtcars)
+
+# limpio los datos
+datos_limpios <- mtcars %>%
+  filter(cyl == 4)
+
+# grafico los datos
+plot(x = mtcars$wt, y = mtcars$mpg)
